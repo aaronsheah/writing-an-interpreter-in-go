d4bfb5a3e4056d91b3a81d9ce3262e729c7d5ba6
Aaron Sheah <aaronsheah@gmail.com>
2020-11-25 19:43:31 +0000

    support mathematical operators

--- monkey/lexer/lexer_test.go
+++ monkey/lexer/lexer_test.go
@@ -133,11 +133,40 @@ func TestNextToken_SupportLogicalOperators(t *testing.T) {
 			},
 		},
 		{
-			`5 - 1;`,
+			`let x = !y;`,
+			[]token.Token{
+				{Type: token.Let, Literal: "let"},
+				{Type: token.Ident, Literal: "x"},
+				{Type: token.Assign, Literal: "="},
+				{Type: token.Bang, Literal: "!"},
+				{Type: token.Ident, Literal: "y"},
+			},
+		},
+	}
+
+	for _, testParam := range testParams {
+		l := New(testParam.Input)
+		checkNextChar(t, l, testParam.ExpectedTokens)
+	}
+}
+
+func TestNextToken_SupportMathematicalOperators(t *testing.T) {
+	testParams := []struct {
+		Input          string
+		ExpectedTokens []token.Token
+	}{
+		{
+			`5 + 4 - 1 * 42 / 69;`,
 			[]token.Token{
 				{Type: token.Int, Literal: "5"},
+				{Type: token.Plus, Literal: "+"},
+				{Type: token.Int, Literal: "4"},
 				{Type: token.Minus, Literal: "-"},
 				{Type: token.Int, Literal: "1"},
+				{Type: token.Asterisk, Literal: "*"},
+				{Type: token.Int, Literal: "42"},
+				{Type: token.Slash, Literal: "/"},
+				{Type: token.Int, Literal: "69"},
 			},
 		},
 	}
